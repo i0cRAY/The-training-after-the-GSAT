00068e440b92a6d44c675d20eea7f11b05bffd6c
0cRAY <166521292+i0cRAY@users.noreply.github.com>
2026-02-17 16:35:21 +0800

    Add main logic for Southeast Asia children's aid

---
+++ 大善人老闆救濟東南亞兒童.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define int long long
+
+using namespace std;
+
+signed main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        stack<int> st;
+        int n;
+        cin >> n;
+        bool can = true;
+        vector<int> goal(n);
+        for (int i = 0; i < n; i++) {
+            cin >> goal[i];
+        }
+
+        int idx = 0;
+        for (int num = 1; num <= n; num++) {
+            if (num == goal[idx]) {
+                idx++;
+            } else {
+                // 看看 st 有沒有
+                while (!st.empty()) {
+                    if (st.top() == goal[idx]) {
+                        idx++;
+                        st.pop();
+                    } else {
+                        break;
+                    }
+                }
+
+                st.push(num);
+            }
+        }
+
+        while (!st.empty()) {
+            if (st.top() == goal[idx]) {
+                idx++;
+                st.pop();
+            } else {
+                // st 還有卻出不去就失敗了
+                can = false;
+                break;
+            }
+        }
+
+        cout << (can ? "Yes" : "No") << "\n";
+    }
+}
